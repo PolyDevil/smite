a2c708e040a7fce8cfb7fd409eec56a05dc51ebd
PolyDevil <polydevil@cocaine.ninja>
2019-04-29 08:06:44 +0300

    Updates

---
+++ precache-manifest.2f2c23fced242fa3674fe6f09dd9bd69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a341d3371fc759e691e5b2ae71a85505",
+    "url": "/smite/index.html"
+  },
+  {
+    "revision": "444cf8fc747f5d33e585",
+    "url": "/smite/static/css/main.887387bc.chunk.css"
+  },
+  {
+    "revision": "b5e1702f67b137fee79e",
+    "url": "/smite/static/js/2.1bd34c75.chunk.js"
+  },
+  {
+    "revision": "444cf8fc747f5d33e585",
+    "url": "/smite/static/js/main.706f7ca4.chunk.js"
+  },
+  {
+    "revision": "5245b64ce292c04af5c6",
+    "url": "/smite/static/js/runtime~main.9202f59d.js"
+  }
+]);
