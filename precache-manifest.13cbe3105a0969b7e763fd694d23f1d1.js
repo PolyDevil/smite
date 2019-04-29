c1b50fe3766a56bf90f0d9112cd78b9c048727f1
Poly Devil <wtf@waifu.club>
2019-04-29 23:12:17 +0300

    Updates

--- precache-manifest.13cbe3105a0969b7e763fd694d23f1d1.js
+++ precache-manifest.13cbe3105a0969b7e763fd694d23f1d1.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "84edac100ec4afe0a8a4bb2d4cf7c6e7",
+    "revision": "5ac038b155e166432ee4fea864386bf7",
     "url": "/smite/index.html"
   },
   {
-    "revision": "df49d9a7fd580e4fc8d4",
-    "url": "/smite/static/css/main.bb6d36fd.chunk.css"
+    "revision": "c02e6abf61fbaf9c6da3",
+    "url": "/smite/static/css/main.5e59127d.chunk.css"
   },
   {
     "revision": "b5e1702f67b137fee79e",
     "url": "/smite/static/js/2.1bd34c75.chunk.js"
   },
   {
-    "revision": "df49d9a7fd580e4fc8d4",
-    "url": "/smite/static/js/main.d3fb1d19.chunk.js"
+    "revision": "c02e6abf61fbaf9c6da3",
+    "url": "/smite/static/js/main.05b21070.chunk.js"
   },
   {
     "revision": "5245b64ce292c04af5c6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/smite/precache-manifest.32dae58f659c170f6850235b57aaa228.js"
+  "/smite/precache-manifest.13cbe3105a0969b7e763fd694d23f1d1.js"
 );
 
 self.addEventListener('message', (event) => {
